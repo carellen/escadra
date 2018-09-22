59b97251e7a40e3b98bb19aa7cc543584583dd02
Vladimir Saponko <saponko@gmail.com>
2018-09-22 11:36:03 +0300

    Add employment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :companies
+  has_many :employments
+  has_many :companies, through: :employments
+
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable, :omniauthable
