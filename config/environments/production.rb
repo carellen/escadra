81f6c2fd0f9875740f9cd1b3e40f508e090a426b
Vladimir Saponko <saponko@gmail.com>
2018-10-28 16:50:17 +0300

    Dockerize deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "escadra_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =
+      {
+        address:              'smtp.gmail.com',
+        port:                 587,
+        domain:               'gmail.com',
+        user_name:            'mail.escadra@gmail.com',
+        password:             'Escadra010203',
+        authentication:       'plain',
+        enable_starttls_auto: true
+      }
+  config.action_mailer.default_url_options = { host: '142.93.187.166' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
